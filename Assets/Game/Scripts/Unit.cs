e23792cd63c3e8367a88aa5b6f390b8a0b91ef24
stephanedurette <stephanedurette@rogers.com>
2026-03-08 10:00:29 -0400

    no message

--- Assets/Game/Scripts/Unit.cs
+++ Assets/Game/Scripts/Unit.cs
@@ -1,13 +1,32 @@
 using Pathfinding;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Unit : MonoBehaviour
 {
+    [HideInInspector] public UnityEvent OnMoveStarted;
+    [HideInInspector] public UnityEvent OnMoveFinished;
+
     private Seeker seeker;
 
     private void Awake()
     {
         seeker = GetComponent<Seeker>();
+    }
+
+    private void OnEnable()
+    {
+        seeker.pathCallback += OnMoveComplete;
+    }
+
+    private void OnDisable()
+    {
+        seeker.pathCallback -= OnMoveComplete;
+    }
+
+    private void OnMoveComplete(Path p)
+    {
+
     }
 
     public void MoveToLocation(Vector2 destination)
---
+++ Assets/Game/Scripts/UnitView.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class UnitView : MonoBehaviour
+{
+    private Animator animator;
+    private Unit unit;
+    private SpriteRenderer spriteRenderer;
+
+    private Vector3 lastPosition;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        unit = GetComponentInParent<Unit>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        unit.OnMoveStarted.AddListener(OnUnitMoveStarted);
+        unit.OnMoveFinished.AddListener(OnUnitMoveFinished);
+    }
+    private void OnDisable()
+    {
+        unit.OnMoveStarted.RemoveListener(OnUnitMoveStarted);
+        unit.OnMoveFinished.RemoveListener(OnUnitMoveFinished);
+    }
+
+    private void Start()
+    {
+        lastPosition = transform.position;
+        animator.SetBool("Walking", false);
+    }
+
+    private void Update()
+    {
+        UpdateSpriteFlip();
+    }
+
+    private void OnUnitMoveStarted()
+    {
+        animator.SetBool("Walking", true);
+    }
+
+    private void OnUnitMoveFinished()
+    {
+        animator.SetBool("Walking", false);
+    }
+
+    private void UpdateSpriteFlip()
+    {
+        int xMoveDirection = Mathf.CeilToInt(transform.position.x - lastPosition.x);
+        spriteRenderer.flipX = xMoveDirection == 0 ? spriteRenderer.flipX : xMoveDirection == -1;
+        lastPosition = transform.position;
+    }
+
+
+}
