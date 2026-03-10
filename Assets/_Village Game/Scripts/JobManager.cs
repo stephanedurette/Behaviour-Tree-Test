def9519b0484cc17001cb55dfe2bf1ff2f34fe8e
stephanedurette <stephanedurette@rogers.com>
2026-03-10 02:43:45 -0400

    no message

---
+++ Assets/_Village Game/Scripts/JobManager.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobManager : MonoBehaviour
+{
+    public void RegisterJob(Job job)
+    {
+
+    }
+
+    public Job FindJob()
+    {
+        return null;
+    }
+
+    public void AssignJob(Unit unit, Job job)
+    {
+
+    }
+}
